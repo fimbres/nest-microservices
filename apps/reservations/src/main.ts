8b211b30d95f414b4036dbac190f519bf43d3e27
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 15:42:04 -0800

    adding validation

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,9 +5,11 @@ import { Logger } from "nestjs-pino";
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
   app.useLogger(app.get(Logger));
-  
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
