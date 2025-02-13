631a9e274b5b984b1c5e0c360f2661258cc45cba
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 20:48:26 -0800

    basic microservices configuration

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URI: Joi.string().required(),
+        AUTH_HOST: Joi.string().required(),
+      })
+    }),
+    // ClientsModule.registerAsync([
+    //   {
+    //     name: AUTH_SERVICE,
+    //     useFactory: (configService: ConfigService) => ({ 
+    //       transport: Transport.TCP,
+    //       options: {
+    //         host: configService.get('AUTH_HOST'),
+    //         port: 3002
+    //       }
+    //     }),
+    //     inject: const [ConfigService],
+    //   }
+    // ])
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
