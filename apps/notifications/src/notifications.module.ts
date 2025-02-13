0879804f68e8d7ec16c6034c65eb7f7702b3100f
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 23:22:03 -0800

    new notification module

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      })
+    })
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
