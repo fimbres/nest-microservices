4d16782c98b974a1c44e34849ea9275ebaf8fb57
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 19:14:36 -0800

    new getCurrentUser method

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../current-user.decorator';
+import { UserDocument } from './models/user.schema';
+import { JwtGuard } from '../guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +12,13 @@ export class UsersController {
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtGuard)
+  async getUser(
+    @CurrentUser() user: UserDocument
+  ) {
+    return user;
   }
 }
