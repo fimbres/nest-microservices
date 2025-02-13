4a411901afd46223e04da7122321a2096353a3fe
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-13 14:50:01 -0800

    adding logger and config

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+
 import { GatewayModule } from './gateway.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
