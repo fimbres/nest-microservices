70997d02cefe48dcdf87ef93600b7a0e75c2517e
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 20:33:14 -0800

    protecting routes

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { JwtGuard, UserDto } from '@app/common';
+
+import { CurrentUser } from 'apps/auth/src/current-user.decorator';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
@@ -7,26 +10,34 @@ import { UpdateReservationDto } from './dto/update-reservation.dto';
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() createReservationDto: CreateReservationDto) {
-    return this.reservationsService.create(createReservationDto);
+  async create(
+    @Body() createReservationDto: CreateReservationDto,
+    @CurrentUser() user: UserDto,
+  ) {
+    return await this.reservationsService.create(createReservationDto, user._id);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   async findAll() {
     return await this.reservationsService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.reservationsService.findOne(id);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
     return await this.reservationsService.update(id, updateReservationDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.reservationsService.remove(id);
