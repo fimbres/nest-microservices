640cd293f182f4be6f50594f8e9bf869dfa06b9a
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 23:22:09 -0800

    sending emails

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+  
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAUTH2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('SMTP_ID'),
+      clientSecret: this.configService.get('SMTP_SECRET'),
+      refreshToken: this.configService.get('SMTP_REFRESH_TOKEN'),
+    }
+  });
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr Reservation',
+      text: 'Success!'
+    });
   }
 }
