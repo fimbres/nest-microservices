98e3a3b3fc6bf2897330b152dfbba3b928c5b0ef
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 22:32:21 -0800

    remove unnecessary code

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -12,26 +12,11 @@ import { PaymentsService } from './payments.service';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        DATABASE_URI: Joi.string().required(),
-        AUTH_HOST: Joi.string().required(),
         PORT: Joi.number().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
         STRIPE_PUBLIC_KEY: Joi.string().required(),
       })
-    }),
-    // ClientsModule.registerAsync([
-    //   {
-    //     name: AUTH_SERVICE,
-    //     useFactory: (configService: ConfigService) => ({ 
-    //       transport: Transport.TCP,
-    //       options: {
-    //         host: configService.get('AUTH_HOST'),
-    //         port: 3002
-    //       }
-    //     }),
-    //     inject: const [ConfigService],
-    //   }
-    // ])
+    })
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
