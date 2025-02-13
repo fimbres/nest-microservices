451976f8240502c1a62e19e6b6c1fcdacc74e3e4
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 20:31:50 -0800

    connecting reservations service with auth service

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { DatabaseModule } from '@app/common';
 import { LoggerModule } from '@app/common/logger';
-import { ConfigModule } from '@nestjs/config';
+import { AUTH_SERVICE } from '@app/common/constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
 import { ReservationsService } from './reservations.service';
@@ -24,7 +26,20 @@ import { ReservationDocument, ReservationSchema } from './models/reservation.sch
       validationSchema: Joi.object({
         DATABASE_URI: Joi.string().required(),
       })
-    })
+    }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({ 
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('AUTH_HOST'),
+            port: 3002
+          }
+        }),
+        inject: [ConfigService],
+      }
+    ])
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
