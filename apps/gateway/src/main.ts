06b0400ddf03a71ce11d106c729e7f97fb47fc71
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-13 15:27:37 -0800

    fixing main

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,11 +3,13 @@ import { Logger } from 'nestjs-pino';
 
 import { GatewayModule } from './gateway.module';
 import { ConfigService } from '@nestjs/config';
+import { setApp } from './app';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
+  setApp(app);
 }
 bootstrap();
