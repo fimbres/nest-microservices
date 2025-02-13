02bcc7faaac52593a331ac4d2d7b674b96669117
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-12 20:32:23 -0800

    new microservice endpoint

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { AuthService } from './auth.service';
 import { LocalGuard } from './guards/local.guard';
 import { CurrentUser } from './current-user.decorator';
 import { UserDocument } from './users/models/user.schema';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Controller()
 export class AuthController {
@@ -19,5 +21,14 @@ export class AuthController {
     await this.authService.login(user, response);
     
     response.send(user);
+  }
+
+  @UseGuards(JwtGuard)
+  @MessagePattern('authenticate')
+  async authenticate(
+    @Payload()
+    data: any
+  ) {
+    return data.user;
   }
 }
